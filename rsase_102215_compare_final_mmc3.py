a0560765cad8979b4589b211954da1f52da384fb
w-saadsaoud <w.saadsaoud@lagh-univ.dz>
2026-08-20 12:46:27 +0100

    Update rsase_102215_compare_final_mmc3.py

--- rsase_102215_compare_final_mmc3.py
+++ rsase_102215_compare_final_mmc3.py
@@ -18,7 +18,7 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-BASE     = r"Chemain données"
+BASE     = r"Your-Path-folder"
 RASTERS  = {
     1990: os.path.join(BASE, "01_DONNEES", "Rasters", "Urban_1990.tif"),
     2010: os.path.join(BASE, "01_DONNEES", "Rasters", "Urban_2010.tif"),
